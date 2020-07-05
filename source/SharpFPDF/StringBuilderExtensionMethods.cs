adae7da2ae20cff2e48d668d0e568160591cdbd2
Mike Küster <mike@xantiva.de>
2020-07-05 13:48:14 +0200

    Add Unit Test, which checks the right new line char

--- source/SharpFPDF/StringBuilderExtensionMethods.cs
+++ source/SharpFPDF/StringBuilderExtensionMethods.cs
@@ -1,14 +1,18 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace SharpFPDF
 {
-    internal static class StringBuilderExtensionMethods
+    public static class StringBuilderExtensionMethods
     {
         private const string pdfNewLine = "\n";
         public static StringBuilder AppendNewLine(this StringBuilder sb, string s)
         {
+            if (sb == null) throw new ArgumentNullException(nameof(sb), "The string builder must not be null.");
+
             sb.Append(s);
             sb.Append(pdfNewLine);
+
             return sb;
         }
     }
---
+++ source/SharpFPDFTests/StringBuilderExtensionMethodsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpFPDF;
+using System.Text;
+
+namespace SharpFPDFTests
+{
+    [TestClass]
+    public class StringBuilderExtensionMethodsTests
+    {
+
+        [TestMethod]
+        public void AppendNewLine()
+        {
+            // arrange
+            var sut = new StringBuilder();
+            var expected = "Hello Pdf\n";
+
+            // act
+            sut.AppendNewLine("Hello Pdf");
+
+            // assert
+            Assert.AreEqual(expected, sut.ToString());
+        }
+    }
+}
